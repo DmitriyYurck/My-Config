ef6e01744a298a5356f3062ba1e30ef7d82f3996
DmitriyYurck <163719243+DmitriyYurck@users.noreply.github.com>
2025-08-16 13:40:15 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- UI Options
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
@@ -14,26 +15,148 @@ vim.g.mapleader = " "
 
 -- Keymaps
 vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("n", "ww", ":w<CR>")
+vim.keymap.set("n", "ww", ":w!<CR>")
 vim.keymap.set("n", "qq", ":q!<CR>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>r", ":Telescope live_grep<CR>") -- replaced grep_string
+vim.keymap.set("n", "<leader>r", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
 vim.keymap.set("n", "<leader>k", ":WhichKey<CR>")
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>") -- Open Terminal
+vim.keymap.set('t', '<leader>jj', [[<C-\><C-n>]])
+vim.keymap.set('t', '<leader>h', [[<C-\>]])
+vim.api.nvim_set_keymap("n", "df", ":noh<CR><Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
 
 -- Theme Toggle
-local current_theme = "poimandres"
-vim.keymap.set("n", "<leader>tt", function()
-  if current_theme == "poimandres" then
-    vim.g.everforest_background = "soft"
-    vim.g.everforest_enable_italic = true
-    vim.cmd("colorscheme everforest")
-    require("lualine").setup({
-      options = {
-        theme = "everforest",
-        section_separators = "",
-        component_separators = ""
+vim.keymap.set("n", "<leader>tt", "<cmd>Telescope colorscheme<CR>", { desc = "Theme picker" })
+-- Lazy.nvim Bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin Setup
+require("lazy").setup({
+  "nvim-tree/nvim-tree.lua",
+  "nvim-treesitter/nvim-treesitter",
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "nvim-lualine/lualine.nvim",
+  "nvim-telescope/telescope.nvim",
+  "mbbill/undotree",
+  { "olivercederborg/poimandres.nvim", lazy = false, priority = 1000 },
+  { "sainnhe/everforest", lazy = false, priority = 1000 },
+  "windwp/nvim-autopairs",
+  "folke/which-key.nvim",
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any options here
+    },
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
+  },
+  { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", config = function() require("bufferline").setup() end },
+  -- ToggleTerm
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 10,
+        open_mapping = [[<c-\>]],
+        direction = "horizontal",
+        shade_terminals = true,
+        start_in_insert = true,
+        persist_size = true,
+      })
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    config = function()
+            require('crates').setup()
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = { 'lua', 'python', 'rust', "cpp", "c" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            }
+    end
+    }
+})
+
+-- Initial Theme Setup
+require("poimandres").setup({})
+vim.cmd.colorscheme "poimandres"
+require("lualine").setup({
+  options = {
+    theme = "poimandres",
+    section_separators = "",
+    component_separators = "",
+  },
+})
+
+-- NvimTree Setup
+require("nvim-tree").setup({
+  view = { width = 30 },
+  renderer = {
+    highlight_git = true,
+    icons = { show = { file = true, folder = true } },
+  },
+})
+
+-- Treesitter Setup
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "lua", "rust", "typescript", "json", "html", "css" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+-- LSP Setup
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+lspconfig.lua_ls.setup({ capabilities = capabilities })
+lspconfig.ts_ls.setup({ capabilities = capabilities })
+lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+-- CMP Setup
+local cmp = require("cmp")
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+  },
+})
+
+-- Misc Plugin Setups
+require("nvim-autopairs").setup({})
+require("telescope").setup({})
+require("which-key").setup({})
       }
     })
     current_theme = "everforest"
